92e7f8649f49a2a0f0e0a91ba9621a98e4c715e2
GustavoDevisloper <morenubrabo@gmail.com>
2024-06-14 01:06:20 -0300

    test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 let subscription;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.post('/subscribe', (req, res) => {
